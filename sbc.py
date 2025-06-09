f0d69a9a2befc35962a9a9c73f4da525f49b59f4
linzer11 <jeonghu949@gmail.com>
2025-06-10 00:05:22 +0900

    Update sbc.py

--- sbc.py
+++ sbc.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.write("🔍 CSV 컬럼 확인:", df.columns.tolist())
 # 데이터셋 로드
 df = pd.read_csv('spotify_tracks.csv')
-
+st.write("🔍 CSV 컬럼 확인:", df.columns.tolist())
